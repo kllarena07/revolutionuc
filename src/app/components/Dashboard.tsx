3fcc85fe9bf9dbe40c2e8b0daab64208c5d85635
warren yun <wyun2006@gmail.com>
2025-03-01 16:18:49 -0500

    haha

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -169,9 +169,10 @@ const CarbonFootprintDashboard = () => {
                 scrollWheelZoom={true}
                 maxBounds={[[-60, -170], [75, 170]]}
                 maxBoundsViscosity={1.0}
+                zoomControl={false}
+                attributionControl={false}
               >
                 <TileLayer
-                  attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
                   url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
                   noWrap={true}
                 />
